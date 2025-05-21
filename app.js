4ac12c503dd0f1e1de342442e7c233f6504075de
Muhammad <mitcao.000@gmail.com>
2025-05-21 16:14:48 +0300

    1-TypeScript-nima

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+let fullName = 'John Doe';
+let isMorried = true;
+let age = 30;
+console.log(fullName);
+console.log(isMorried);
+console.log(age);
---
+++ app.ts
@@ -0,0 +1,7 @@
+let fullName: string = 'John Doe'
+let isMorried: boolean = true
+let age: number = 30
+
+console.log(fullName)
+console.log(isMorried)
+console.log(age)
