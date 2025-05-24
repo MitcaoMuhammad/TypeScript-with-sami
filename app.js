4978579c9dd564ecc36671c4e038c768a4ef4ca1
Muhammad <mitcao.000@gmail.com>
2025-05-24 16:03:16 +0300

    8-Enums

--- app.js
+++ app.js
@@ -1,8 +1,57 @@
 "use strict";
-let numbers = [1, 2, 3, 4, 5];
-// let person: readonly [string, number] = ['John Doe', 30]
-// const [fullname, age] = person
-// console.log(fullname)
-// console.log(age)
-// let admins: readonly number[] = [1, 2, 3, 4, 5]
-// console.log(admins)
+// enum Direction {
+// 	Up,
+// 	Down,
+// 	Left,
+// 	Right,
+// }
+// enum Direction {
+// 	Up = 1,
+// 	Down = 2,
+// 	Left = 3,
+// 	Right = 4,
+// }
+// enum Direction {
+// 	Up = 'UP',
+// 	Down = 'DOWN',
+// 	Left = 'LEFT',
+// 	Right = 'RIGHT',
+// }
+// const move: Direction = Direction.Up
+// console.log(move)
+// move = Direction.Down
+// console.log(move)
+// function getDirection(direction: Direction) {
+// 	if (direction === Direction.Up) {
+// 		return 'Moving Up'
+// 	} else if (direction === Direction.Down) {
+// 		return 'Moving Down'
+// 	} else if (direction === Direction.Left) {
+// 		return 'Moving Left'
+// 	} else if (direction === Direction.Right) {
+// 		return 'Moving Right'
+// 	}
+// }
+// const moveUp = getDirection(Direction.Up)
+// console.log(moveUp)
+// const moveDown = getDirection(Direction.Down)
+// console.log(moveDown)
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["OK"] = 200] = "OK";
+    StatusCode[StatusCode["NOT_FOUND"] = 404] = "NOT_FOUND";
+    StatusCode[StatusCode["INTERNAL_SERVER_ERROR"] = 500] = "INTERNAL_SERVER_ERROR";
+})(StatusCode || (StatusCode = {}));
+function handleRequest(url) {
+    if (url === '/') {
+        return StatusCode.OK;
+    }
+    else if (url === '/not-found') {
+        return StatusCode.NOT_FOUND;
+    }
+    else {
+        return StatusCode.INTERNAL_SERVER_ERROR;
+    }
+}
+const homePageStatus = handleRequest('/');
+console.log(homePageStatus);
--- app.ts
+++ app.ts
@@ -1,12 +1,62 @@
-let numbers: ReadonlyArray<number> = [1, 2, 3, 4, 5]
+// enum Direction {
+// 	Up,
+// 	Down,
+// 	Left,
+// 	Right,
+// }
 
-// let person: readonly [string, number] = ['John Doe', 30]
+// enum Direction {
+// 	Up = 1,
+// 	Down = 2,
+// 	Left = 3,
+// 	Right = 4,
+// }
 
-// const [fullname, age] = person
+// enum Direction {
+// 	Up = 'UP',
+// 	Down = 'DOWN',
+// 	Left = 'LEFT',
+// 	Right = 'RIGHT',
+// }
 
-// console.log(fullname)
-// console.log(age)
+// const move: Direction = Direction.Up
+// console.log(move)
 
-// let admins: readonly number[] = [1, 2, 3, 4, 5]
+// move = Direction.Down
+// console.log(move)
 
-// console.log(admins)
+// function getDirection(direction: Direction) {
+// 	if (direction === Direction.Up) {
+// 		return 'Moving Up'
+// 	} else if (direction === Direction.Down) {
+// 		return 'Moving Down'
+// 	} else if (direction === Direction.Left) {
+// 		return 'Moving Left'
+// 	} else if (direction === Direction.Right) {
+// 		return 'Moving Right'
+// 	}
+// }
+
+// const moveUp = getDirection(Direction.Up)
+// console.log(moveUp)
+// const moveDown = getDirection(Direction.Down)
+// console.log(moveDown)
+
+enum StatusCode {
+	OK = 200,
+	NOT_FOUND = 404,
+	INTERNAL_SERVER_ERROR = 500,
+}
+
+function handleRequest(url: string): StatusCode {
+	if (url === '/') {
+		return StatusCode.OK
+	} else if (url === '/not-found') {
+		return StatusCode.NOT_FOUND
+	} else {
+		return StatusCode.INTERNAL_SERVER_ERROR
+	}
+}
+
+const homePageStatus = handleRequest('/')
+console.log(homePageStatus)
