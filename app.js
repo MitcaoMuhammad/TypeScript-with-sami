2b98107a4f7a14b535d22675a118c2072505ba4e
Muhammad <mitcao.000@gmail.com>
2025-05-25 11:00:08 +0300

    10-Union-type

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
 "use strict";
-var website;
-(function (website) {
-    website["facebook"] = "https://www.facebook.com";
-    website["twitter"] = "https://www.twitter.com";
-    website["instagram"] = "https://www.instagram.com";
-    website["youtube"] = "https://www.youtube.com";
-})(website || (website = {}));
-let person = {
-    id: 1,
-    name: 'Leanne Graham',
-    isMarried: false,
-    username: 'Bret',
-    email: 'Sincere@april.biz',
-    address: {
-        street: 'Kulas Light',
-        suite: 'Apt. 556',
-        city: 'Gwenborough',
-        zipcode: 929983874,
-        geo: [-37.3159, 81.1496],
-    },
-    phone: 17707368031,
-    website: website.facebook,
-    company: {
-        name: 'Romaguera-Crona',
-        catchPhrase: 'Multi-layered client-server neural-net',
-        bs: 'harness real-time e-markets',
-    },
-};
+let data = ['Alice', 30, 'Bob', 25, 'Charlie', 35];
+// Type Narrowing - turni tekshirish
+// function logError(error: { message: string } | { error: string }) {
+// 	if ('message' in error) {
+// 		console.log(`Error message: ${error.message}`)
+// 	} else {
+// 		console.log(`Error: ${error.error}`)
+// 	}
+// }
+// logError({ message: 'Something went wrong!' })
+// logError({ error: 'Failed to connect to the server.' })
+// function getVal(value: string | number): string | number {
+// 	if (typeof value === 'string') {
+// 		return value.toUpperCase()
+// 	} else if (typeof value === 'number') {
+// 		return value.toFixed(2)
+// 	}
+// 	return `Unknown type: ${value}`
+// }
+// console.log(getVal('hello'))
+// console.log(getVal(123.456))
+// function printID(id: number | string): void {
+// 	console.log(`The ID is: ${id}`)
+// }
+// printID(101)
+// printID('202A')
--- app.ts
+++ app.ts
@@ -1,48 +1,34 @@
-enum website {
-	facebook = 'https://www.facebook.com',
-	twitter = 'https://www.twitter.com',
-	instagram = 'https://www.instagram.com',
-	youtube = 'https://www.youtube.com',
-}
+let data: (string | number)[] = ['Alice', 30, 'Bob', 25, 'Charlie', 35]
 
-let person: {
-	id: number
-	name: string
-	isMarried: boolean
-	username: string
-	email: string
-	address: {
-		street: string
-		suite: string
-		city: string
-		zipcode: string | number
-		geo: [number, number]
-	}
-	phone: number
-	website: website
-	company: {
-		name: string
-		catchPhrase: string
-		bs: string
-	}
-} = {
-	id: 1,
-	name: 'Leanne Graham',
-	isMarried: false,
-	username: 'Bret',
-	email: 'Sincere@april.biz',
-	address: {
-		street: 'Kulas Light',
-		suite: 'Apt. 556',
-		city: 'Gwenborough',
-		zipcode: 929983874,
-		geo: [-37.3159, 81.1496],
-	},
-	phone: 17707368031,
-	website: website.facebook,
-	company: {
-		name: 'Romaguera-Crona',
-		catchPhrase: 'Multi-layered client-server neural-net',
-		bs: 'harness real-time e-markets',
-	},
-}
+// Type Narrowing - turni tekshirish
+
+// function logError(error: { message: string } | { error: string }) {
+// 	if ('message' in error) {
+// 		console.log(`Error message: ${error.message}`)
+// 	} else {
+// 		console.log(`Error: ${error.error}`)
+// 	}
+// }
+
+// logError({ message: 'Something went wrong!' })
+// logError({ error: 'Failed to connect to the server.' })
+
+// function getVal(value: string | number): string | number {
+// 	if (typeof value === 'string') {
+// 		return value.toUpperCase()
+// 	} else if (typeof value === 'number') {
+// 		return value.toFixed(2)
+// 	}
+
+// 	return `Unknown type: ${value}`
+// }
+
+// console.log(getVal('hello'))
+// console.log(getVal(123.456))
+
+// function printID(id: number | string): void {
+// 	console.log(`The ID is: ${id}`)
+// }
+
+// printID(101)
+// printID('202A')
