7525711110460cf5c2950606189e9dc4f263ecee
Muhammad <mitcao.000@gmail.com>
2025-05-26 14:56:14 +0300

    20-Create-class

--- app.js
+++ app.js
@@ -1,81 +1,13 @@
 "use strict";
-var Role;
-(function (Role) {
-    Role[Role["Admin"] = 0] = "Admin";
-    Role[Role["STUDENT"] = 1] = "STUDENT";
-})(Role || (Role = {}));
-function isAdmin(user) {
-    return user.role === Role.Admin;
-}
-const courses = [];
-function addCourse(user, course) {
-    if (isAdmin(user)) {
-        courses.push(course);
-        console.log(`Course added ${course.title} `);
-    }
-    else {
-        console.log('only admin can add courses');
+class Car {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
     }
 }
-function enrollStudent(user, courseId) {
-    const course = courses.find(course => course.id === courseId);
-    if (!course) {
-        console.log('Course not found');
-        return;
-    }
-    if (user.role === Role.STUDENT) {
-        course.students.push(user);
-        console.log(`student enrolled ${user.name}`);
-    }
-    else {
-        console.log('only students can enroll');
-    }
-}
-function listStudents(user, courseId) {
-    if (!isAdmin(user)) {
-        console.log('only admin can see list students');
-        return;
-    }
-    const course = courses.find(course => course.id === courseId);
-    if (!course) {
-        console.log('course not found');
-        return;
-    }
-    console.log(`students in  ${course.title}: ${course.students
-        .map(s => s.name)
-        .join(', ')}`);
-}
-// data
-const admin = {
-    id: 1,
-    name: 'Admin User',
-    role: Role.Admin,
-};
-const student1 = {
-    id: 2,
-    name: 'Student One',
-    role: Role.STUDENT,
-};
-const student2 = {
-    id: 3,
-    name: 'Student Two',
-    role: Role.STUDENT,
-};
-const course1 = {
-    id: 1,
-    title: 'TypeScript Basics',
-    description: 'Learn the basics of TypeScript',
-    students: [],
-};
-const course2 = {
-    id: 2,
-    title: 'Advanced TypeScript',
-    description: 'Deep dive into TypeScript features',
-    students: [],
-};
-// calling functions
-addCourse(admin, course1);
-enrollStudent(student1, 1);
-enrollStudent(student2, 1);
-listStudents(admin, 1);
-console.log(courses);
+const toyota = new Car('Toyota', new Date(2020, 0, 1));
+console.log(toyota);
+toyota.name = 'Toyota Corolla';
+console.log(toyota);
+const chevrolet = new Car('Chevrolet', new Date(2021, 0, 1));
+console.log(chevrolet);
--- app.ts
+++ app.ts
@@ -1,105 +1,17 @@
-enum Role {
-	Admin,
-	STUDENT,
-}
-
-interface IUser {
-	id: number
+class Car {
 	name: string
-	role: Role
-}
+	year: Date
 
-interface ICourse {
-	id: number
-	title: string
-	description: string
-	students: IUser[]
-}
-
-function isAdmin(user: IUser): user is IUser & { role: Role.Admin } {
-	return user.role === Role.Admin
-}
-
-const courses: ICourse[] = []
-
-function addCourse(user: IUser, course: ICourse) {
-	if (isAdmin(user)) {
-		courses.push(course)
-		console.log(`Course added ${course.title} `)
-	} else {
-		console.log('only admin can add courses')
+	constructor(name: string, year: Date) {
+		this.name = name
+		this.year = year
 	}
 }
 
-function enrollStudent(user: IUser, courseId: number) {
-	const course = courses.find(course => course.id === courseId)
+const toyota = new Car('Toyota', new Date(2020, 0, 1))
+console.log(toyota)
+toyota.name = 'Toyota Corolla'
+console.log(toyota)
 
-	if (!course) {
-		console.log('Course not found')
-		return
-	}
-
-	if (user.role === Role.STUDENT) {
-		course.students.push(user)
-		console.log(`student enrolled ${user.name}`)
-	} else {
-		console.log('only students can enroll')
-	}
-}
-
-function listStudents(user: IUser, courseId: number) {
-	if (!isAdmin(user)) {
-		console.log('only admin can see list students')
-		return
-	}
-
-	const course = courses.find(course => course.id === courseId)
-	if (!course) {
-		console.log('course not found')
-		return
-	}
-
-	console.log(
-		`students in  ${course.title}: ${course.students
-			.map(s => s.name)
-			.join(', ')}`
-	)
-}
-
-// data
-const admin: IUser = {
-	id: 1,
-	name: 'Admin User',
-	role: Role.Admin,
-}
-const student1: IUser = {
-	id: 2,
-	name: 'Student One',
-	role: Role.STUDENT,
-}
-const student2: IUser = {
-	id: 3,
-	name: 'Student Two',
-	role: Role.STUDENT,
-}
-const course1: ICourse = {
-	id: 1,
-	title: 'TypeScript Basics',
-	description: 'Learn the basics of TypeScript',
-	students: [],
-}
-const course2: ICourse = {
-	id: 2,
-	title: 'Advanced TypeScript',
-	description: 'Deep dive into TypeScript features',
-	students: [],
-}
-
-// calling functions
-addCourse(admin, course1)
-
-enrollStudent(student1, 1)
-enrollStudent(student2, 1)
-listStudents(admin, 1)
-
-console.log(courses)
+const chevrolet = new Car('Chevrolet', new Date(2021, 0, 1))
+console.log(chevrolet)
