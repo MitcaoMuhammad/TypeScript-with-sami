80c6d304aaeb866db459f7e06afaef0e71ea0f9b
Muhammad <mitcao.000@gmail.com>
2025-05-27 16:42:37 +0300

    30-This

---
+++ app.ts
@@ -0,0 +1,38 @@
+class Calculator {
+	value: number
+
+	add(num: number) {
+		this.value += num
+		return this
+	}
+
+	subtract(num: number) {
+		this.value -= num
+		return this
+	}
+
+	multiply(num: number) {
+		this.value *= num
+		return this
+	}
+
+	getValue() {
+		return this.value
+	}
+}
+
+const calculator = new Calculator()
+const res = calculator.add(10).subtract(5).multiply(2).getValue() // 10
+console.log(res) // 10
+
+// class Counter {
+// 	count: number
+
+// 	increment() {
+// 		console.log(this)
+// 	}
+// }
+
+// const c = new Counter()
+// const incrementFN = c.increment
+// incrementFN() // undefined
