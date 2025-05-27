8dd594ddea5cc4e0061ea4099668cf0beb461a6f
Muhammad <mitcao.000@gmail.com>
2025-05-27 16:41:49 +0300

    30-This

--- app.js
+++ app.js
@@ -1,50 +1,30 @@
 "use strict";
-class Animal {
-    constructor(name) {
-        this.name = name;
+class Calculator {
+    add(num) {
+        this.value += num;
+        return this;
     }
-    move() {
-        console.log(`${this.name} is moving`);
+    subtract(num) {
+        this.value -= num;
+        return this;
+    }
+    multiply(num) {
+        this.value *= num;
+        return this;
+    }
+    getValue() {
+        return this.value;
     }
 }
-class Cat extends Animal {
-    makeSound() {
-        console.log(`${this.name} says Meow`);
-    }
-}
-const cat = new Cat('Tom');
-cat.makeSound(); // Tom says Meow
-cat.move(); // Tom is moving
-// class Parent {
-// 	static message: string = 'Hello from Parent class'
-// }
-// class Child extends Parent {}
-// console.log(Child.message) // Accessing static property from Child class
+const calculator = new Calculator();
+const res = calculator.add(10).subtract(5).multiply(2).getValue(); // 10
+console.log(res); // 10
 // class Counter {
-// 	static count = 0
-// 	private static secretKey: string = '123456'
-// 	title: string
-// 	constructor(title: string) {
-// 		this.title = title
-// 	}
-// 	static increment() {
-// 		this.count++
-// 	}
-// 	static add(a: number, b: number) {
-// 		return a + b
-// 	}
-// 	static getSecretKey() {
-// 		return this.secretKey
-// 	}
-// 	gerTitle() {
-// 		return this.title
+// 	count: number
+// 	increment() {
+// 		console.log(this)
 // 	}
 // }
-// const c = new Counter('Counter 1')
-// console.log(c.title)
-// console.log(Counter.count)
-// Counter.increment()
-// console.log(Counter.count)
-// console.log(Counter.add(5, 10))
-// console.log(Counter.count)
-// console.log(Counter.getSecretKey())
+// const c = new Counter()
+// const incrementFN = c.increment
+// incrementFN() // undefined
