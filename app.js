4f596e7bfbdd72a0edb4169a132f76253667e9c5
Muhammad <mitcao.000@gmail.com>
2025-05-28 13:42:33 +0300

    31-Amaliyot

--- app.js
+++ app.js
@@ -1,30 +1,65 @@
 "use strict";
-class Calculator {
-    add(num) {
-        this.value += num;
-        return this;
-    }
-    subtract(num) {
-        this.value -= num;
-        return this;
-    }
-    multiply(num) {
-        this.value *= num;
-        return this;
-    }
-    getValue() {
-        return this.value;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Role = void 0;
+var Role;
+(function (Role) {
+    Role[Role["Admin"] = 0] = "Admin";
+    Role[Role["STUDENT"] = 1] = "STUDENT";
+})(Role || (exports.Role = Role = {}));
+class User {
+    constructor(id, name, role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
     }
 }
-const calculator = new Calculator();
-const res = calculator.add(10).subtract(5).multiply(2).getValue(); // 10
-console.log(res); // 10
-// class Counter {
-// 	count: number
-// 	increment() {
-// 		console.log(this)
-// 	}
-// }
-// const c = new Counter()
-// const incrementFN = c.increment
-// incrementFN() // undefined
+class Course {
+    constructor(id, title, description) {
+        this.students = [];
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+    get studentList() {
+        return this.students.map(student => student.name).join(', ');
+    }
+    enrollStudent(student) {
+        if (student.role === Role.STUDENT) {
+            this.students.push(student);
+            console.log(`Student ${student.name} enrolled in course ${this.title}`);
+        }
+        else {
+            console.log('Only students can enroll in courses');
+        }
+    }
+}
+class Admin extends User {
+    constructor(id, name) {
+        super(id, name, Role.Admin);
+    }
+    addCourse(course, courses) {
+        courses.push(course);
+        console.log(`Course added: ${course.title}`);
+    }
+    getStudentList(course) {
+        console.log(`Students in course ${course.title}: ${course.studentList}`);
+    }
+}
+class Student extends User {
+    constructor(id, name) {
+        super(id, name, Role.STUDENT);
+    }
+    enrollInCourse(course) {
+        course.enrollStudent(this);
+    }
+}
+// Backtest
+const courses = [];
+const admin = new Admin(1, 'Admin');
+const student1 = new Student(2, 'Student 1');
+const student2 = new Student(3, 'Student 2');
+const course2 = new Course(2, 'Advanced TypeScript', 'Deep dive into TypeScript features');
+admin.addCourse(course2, courses);
+student1.enrollInCourse(course2);
+student2.enrollInCourse(course2);
+admin.getStudentList(course2);
--- app.ts
+++ app.ts
@@ -1,38 +1,87 @@
-class Calculator {
-	value: number
+export enum Role {
+	Admin,
+	STUDENT,
+}
 
-	add(num: number) {
-		this.value += num
-		return this
-	}
+class User {
+	id: number
+	name: string
+	role: Role
 
-	subtract(num: number) {
-		this.value -= num
-		return this
-	}
-
-	multiply(num: number) {
-		this.value *= num
-		return this
-	}
-
-	getValue() {
-		return this.value
+	constructor(id: number, name: string, role: Role) {
+		this.id = id
+		this.name = name
+		this.role = role
 	}
 }
 
-const calculator = new Calculator()
-const res = calculator.add(10).subtract(5).multiply(2).getValue() // 10
-console.log(res) // 10
+class Course {
+	private students: User[] = []
+	id: number
+	title: string
+	description: string
 
-// class Counter {
-// 	count: number
+	constructor(id: number, title: string, description: string) {
+		this.id = id
+		this.title = title
+		this.description = description
+	}
 
-// 	increment() {
-// 		console.log(this)
-// 	}
-// }
+	get studentList(): string {
+		return this.students.map(student => student.name).join(', ')
+	}
 
-// const c = new Counter()
-// const incrementFN = c.increment
-// incrementFN() // undefined
+	enrollStudent(student: User): void {
+		if (student.role === Role.STUDENT) {
+			this.students.push(student)
+			console.log(`Student ${student.name} enrolled in course ${this.title}`)
+		} else {
+			console.log('Only students can enroll in courses')
+		}
+	}
+}
+
+class Admin extends User {
+	constructor(id: number, name: string) {
+		super(id, name, Role.Admin)
+	}
+
+	addCourse(course: Course, courses: Course[]): void {
+		courses.push(course)
+		console.log(`Course added: ${course.title}`)
+	}
+
+	getStudentList(course: Course): void {
+		console.log(`Students in course ${course.title}: ${course.studentList}`)
+	}
+}
+
+class Student extends User {
+	constructor(id: number, name: string) {
+		super(id, name, Role.STUDENT)
+	}
+
+	enrollInCourse(course: Course): void {
+		course.enrollStudent(this)
+	}
+}
+
+// Backtest
+
+const courses: Course[] = []
+const admin = new Admin(1, 'Admin')
+const student1 = new Student(2, 'Student 1')
+const student2 = new Student(3, 'Student 2')
+
+const course2 = new Course(
+	2,
+	'Advanced TypeScript',
+	'Deep dive into TypeScript features'
+)
+
+admin.addCourse(course2, courses)
+
+student1.enrollInCourse(course2)
+student2.enrollInCourse(course2)
+
+admin.getStudentList(course2)
