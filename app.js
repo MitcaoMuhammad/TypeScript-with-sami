2516df6f60eecc36aac8d34bab19a8a2a5494474
Muhammad <mitcao.000@gmail.com>
2025-05-28 14:31:15 +0300

    32-Generic-nima

--- app.js
+++ app.js
@@ -1,65 +1,22 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Role = void 0;
-var Role;
-(function (Role) {
-    Role[Role["Admin"] = 0] = "Admin";
-    Role[Role["STUDENT"] = 1] = "STUDENT";
-})(Role || (exports.Role = Role = {}));
-class User {
-    constructor(id, name, role) {
-        this.id = id;
-        this.name = name;
-        this.role = role;
-    }
-}
-class Course {
-    constructor(id, title, description) {
-        this.students = [];
-        this.id = id;
-        this.title = title;
-        this.description = description;
-    }
-    get studentList() {
-        return this.students.map(student => student.name).join(', ');
-    }
-    enrollStudent(student) {
-        if (student.role === Role.STUDENT) {
-            this.students.push(student);
-            console.log(`Student ${student.name} enrolled in course ${this.title}`);
-        }
-        else {
-            console.log('Only students can enroll in courses');
-        }
-    }
-}
-class Admin extends User {
-    constructor(id, name) {
-        super(id, name, Role.Admin);
-    }
-    addCourse(course, courses) {
-        courses.push(course);
-        console.log(`Course added: ${course.title}`);
-    }
-    getStudentList(course) {
-        console.log(`Students in course ${course.title}: ${course.studentList}`);
-    }
-}
-class Student extends User {
-    constructor(id, name) {
-        super(id, name, Role.STUDENT);
-    }
-    enrollInCourse(course) {
-        course.enrollStudent(this);
-    }
-}
-// Backtest
-const courses = [];
-const admin = new Admin(1, 'Admin');
-const student1 = new Student(2, 'Student 1');
-const student2 = new Student(3, 'Student 2');
-const course2 = new Course(2, 'Advanced TypeScript', 'Deep dive into TypeScript features');
-admin.addCourse(course2, courses);
-student1.enrollInCourse(course2);
-student2.enrollInCourse(course2);
-admin.getStudentList(course2);
+var Rouls;
+(function (Rouls) {
+    Rouls[Rouls["ADMIN"] = 0] = "ADMIN";
+    Rouls[Rouls["USER"] = 1] = "USER";
+})(Rouls || (Rouls = {}));
+const roules = {
+    [Rouls.ADMIN]: 'admin',
+    [Rouls.USER]: 'user',
+};
+const users = [
+    {
+        name: 'John',
+        age: 30,
+    },
+    {
+        name: 'Jane',
+        age: 25,
+    },
+];
+const numbers = [1, 2, 3, 4, 5];
+const names = ['John', 30];
--- app.ts
+++ app.ts
@@ -1,87 +1,31 @@
-export enum Role {
-	Admin,
-	STUDENT,
+enum Rouls {
+	ADMIN,
+	USER,
 }
 
-class User {
-	id: number
+type UserRoules = Record<string, string>
+
+const roules: UserRoules = {
+	[Rouls.ADMIN]: 'admin',
+	[Rouls.USER]: 'user',
+}
+
+interface IUser {
 	name: string
-	role: Role
 
-	constructor(id: number, name: string, role: Role) {
-		this.id = id
-		this.name = name
-		this.role = role
-	}
+	age: number
 }
 
-class Course {
-	private students: User[] = []
-	id: number
-	title: string
-	description: string
+const users: Array<IUser> = [
+	{
+		name: 'John',
+		age: 30,
+	},
+	{
+		name: 'Jane',
+		age: 25,
+	},
+]
 
-	constructor(id: number, title: string, description: string) {
-		this.id = id
-		this.title = title
-		this.description = description
-	}
-
-	get studentList(): string {
-		return this.students.map(student => student.name).join(', ')
-	}
-
-	enrollStudent(student: User): void {
-		if (student.role === Role.STUDENT) {
-			this.students.push(student)
-			console.log(`Student ${student.name} enrolled in course ${this.title}`)
-		} else {
-			console.log('Only students can enroll in courses')
-		}
-	}
-}
-
-class Admin extends User {
-	constructor(id: number, name: string) {
-		super(id, name, Role.Admin)
-	}
-
-	addCourse(course: Course, courses: Course[]): void {
-		courses.push(course)
-		console.log(`Course added: ${course.title}`)
-	}
-
-	getStudentList(course: Course): void {
-		console.log(`Students in course ${course.title}: ${course.studentList}`)
-	}
-}
-
-class Student extends User {
-	constructor(id: number, name: string) {
-		super(id, name, Role.STUDENT)
-	}
-
-	enrollInCourse(course: Course): void {
-		course.enrollStudent(this)
-	}
-}
-
-// Backtest
-
-const courses: Course[] = []
-const admin = new Admin(1, 'Admin')
-const student1 = new Student(2, 'Student 1')
-const student2 = new Student(3, 'Student 2')
-
-const course2 = new Course(
-	2,
-	'Advanced TypeScript',
-	'Deep dive into TypeScript features'
-)
-
-admin.addCourse(course2, courses)
-
-student1.enrollInCourse(course2)
-student2.enrollInCourse(course2)
-
-admin.getStudentList(course2)
+const numbers: Array<number> = [1, 2, 3, 4, 5]
+const names: [string, number] = ['John', 30]
