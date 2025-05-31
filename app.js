e48ad557c4c27fc48bb5c2198460b3494e13f9d4
Muhammad <mitcao.000@gmail.com>
2025-05-31 05:57:07 +0300

    40-Readonly-Partial-Required

--- app.js
+++ app.js
@@ -1,49 +1,22 @@
 "use strict";
-function validateFormData(form) {
-    const errors = {};
-    if (!form.email) {
-        errors.email = 'Email is required';
-    }
-    else if (!/\S+@\S+\.\S+/.test(form.email)) {
-        errors.email = 'Email is invalid';
-    }
-    if (!form.password) {
-        errors.password = 'Password is required';
-    }
-    else if (form.password.length < 6) {
-        errors.password = 'Password must be at least 6 characters long';
-    }
-    if (!form.name) {
-        errors.name = 'Name is required';
-    }
-    return errors;
-}
-const formData = {
-    email: 'exemple@gmail.com',
-    password: '123456',
-    name: 'John Doe',
+const product = {
+    name: 'shoes',
+    price: 100,
+    id: 1,
+    excerpt: 'A pair of running shoes',
 };
-const errors = validateFormData(formData);
-console.log(errors);
-function isEmailValid(form) {
-    return /\S+@\S+\.\S+/.test(form.email);
-}
-const emailValidation = isEmailValid(formData);
-console.log(emailValidation); // true
-function validateDynamicFormData(form) {
-    const errors = {};
-    Object.keys(form).forEach(key => {
-        if (!form[key]) {
-            errors[key] = `${key} is required`;
-        }
-    });
-    return errors;
-}
-const dynamicFormData = {
-    email: 'test@gmail.com',
-    password: '123456',
-    name: 'John Doe',
-    customId: '',
-};
-const dynamicErrors = validateDynamicFormData(dynamicFormData);
-console.log(dynamicErrors); // { customId: 'customId is required' }
+// const products = {
+// 	name: 'shoes',
+// 	price: 100,
+// 	id: 1,
+// 	excerpt: 'A pair of running shoes',
+// }
+// function createProduct(products: Required<IProduct>) {
+// 	console.log(products)
+// }
+// createProduct(products)
+// function updateProduct(updateProduct: Partial<IProduct>) {
+// 	const updatedProduct: IProduct = { ...products, ...updateProduct }
+// 	console.log(updatedProduct)
+// }
+// updateProduct({ name: 'sneakers', price: 120 })
--- app.ts
+++ app.ts
@@ -1,77 +1,35 @@
-type FormDataType = {
-	email: string
-	password: string
+interface IProduct {
 	name: string
+	price: number
+	id: number
+	excerpt?: string
 }
 
-type FormErrorType = {
-	email?: string
-	password?: string
-	name?: string
+type ReadonlyProduct = Readonly<IProduct>
+
+const product: ReadonlyProduct = {
+	name: 'shoes',
+	price: 100,
+	id: 1,
+	excerpt: 'A pair of running shoes',
 }
 
-function validateFormData<T extends FormDataType>(form: T): FormErrorType {
-	const errors: FormErrorType = {}
+// const products = {
+// 	name: 'shoes',
+// 	price: 100,
+// 	id: 1,
+// 	excerpt: 'A pair of running shoes',
+// }
 
-	if (!form.email) {
-		errors.email = 'Email is required'
-	} else if (!/\S+@\S+\.\S+/.test(form.email)) {
-		errors.email = 'Email is invalid'
-	}
+// function createProduct(products: Required<IProduct>) {
+// 	console.log(products)
+// }
 
-	if (!form.password) {
-		errors.password = 'Password is required'
-	} else if (form.password.length < 6) {
-		errors.password = 'Password must be at least 6 characters long'
-	}
+// createProduct(products)
 
-	if (!form.name) {
-		errors.name = 'Name is required'
-	}
+// function updateProduct(updateProduct: Partial<IProduct>) {
+// 	const updatedProduct: IProduct = { ...products, ...updateProduct }
+// 	console.log(updatedProduct)
+// }
 
-	return errors
-}
-
-const formData: FormDataType = {
-	email: 'exemple@gmail.com',
-	password: '123456',
-	name: 'John Doe',
-}
-
-const errors = validateFormData(formData)
-console.log(errors)
-
-type IsEmailValid<T> = T extends { email: string } ? boolean : never
-
-function isEmailValid<T extends FormDataType>(form: T): IsEmailValid<T> {
-	return /\S+@\S+\.\S+/.test(form.email) as IsEmailValid<T>
-}
-
-const emailValidation = isEmailValid(formData)
-console.log(emailValidation) // true
-
-type DynamicFormData = {
-	[key: string]: string
-}
-
-function validateDynamicFormData<T extends DynamicFormData>(
-	form: T
-): FormErrorType {
-	const errors: FormErrorType = {}
-	Object.keys(form).forEach(key => {
-		if (!form[key]) {
-			errors[key] = `${key} is required`
-		}
-	})
-	return errors
-}
-
-const dynamicFormData = {
-	email: 'test@gmail.com',
-	password: '123456',
-	name: 'John Doe',
-	customId: '',
-}
-
-const dynamicErrors = validateDynamicFormData(dynamicFormData)
-console.log(dynamicErrors) // { customId: 'customId is required' }
+// updateProduct({ name: 'sneakers', price: 120 })
