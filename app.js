9fac7e3461c333a31481584433e7083ae58c9feb
Muhammad <mitcao.000@gmail.com>
2025-05-31 05:00:24 +0300

    37-Keyof-and-typeof

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 "use strict";
-class Shape {
-    constructor(content, radius) {
-        this.content = content;
-        this.radius = radius;
-    }
-    getContent() {
-        return this.content;
-    }
-    getRadius() {
-        return this.radius;
-    }
+const config = {
+    apiUrl: 'https://api.example.com',
+    port: 3000,
+    debug: true,
+};
+function getConfigValue(key) {
+    return config[key];
 }
-const shape = new Shape('Circle', 5);
-console.log(shape.getContent()); // Output: Circle
-const shape2 = new Shape(10, 'Large');
-console.log(shape2.getRadius()); // Output: Large
+console.log(getConfigValue('apiUrl')); // 'https://api.example.com'
+console.log(getConfigValue('port')); // 3000
+// typeof - qiymatning turini olish uchun ishlatiladi
+// keyof - obyektning kalitlarini olish uchun ishlatiladi
+// keyof typeof - obyektning kalitlarini olish uchun ishlatiladi, lekin obyektning turini ham aniqlaydi
+// const user = {
+// 	name: 'John Doe',
+// 	age: 30,
+// 	isMarried: false,
+// }
+// type User = typeof user
+// function logUser(user: User) {
+// 	console.log(user)
+// }
+// logUser(user)
+// type IUser = {
+// 	name: string
+// 	age: number
+// 	isMarried: boolean
+// }
+// type UserKeys = keyof IUser
+// function getUserKey(key: UserKeys) {
+// 	console.log(key)
+// }
+// getUserKey('age')
+// function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+// 	return obj[key]
+// }
+// const car = {
+// 	modal: 'Toyota',
+// 	year: 2020,
+// 	color: 'red',
+// }
+// const userName = getProperty(car, 'year')
--- app.ts
+++ app.ts
@@ -1,23 +1,58 @@
-class Shape<T, U> {
-	private content: T
-	radius: U
-
-	constructor(content: T, radius: U) {
-		this.content = content
-		this.radius = radius
-	}
-
-	getContent(): T {
-		return this.content
-	}
-
-	getRadius(): U {
-		return this.radius
-	}
+const config = {
+	apiUrl: 'https://api.example.com',
+	port: 3000,
+	debug: true,
 }
 
-const shape = new Shape<string, number>('Circle', 5)
-console.log(shape.getContent()) // Output: Circle
+type ConfigKeys = keyof typeof config
 
-const shape2 = new Shape<number, string>(10, 'Large')
-console.log(shape2.getRadius()) // Output: Large
+function getConfigValue(key: ConfigKeys) {
+	return config[key]
+}
+
+console.log(getConfigValue('apiUrl')) // 'https://api.example.com'
+console.log(getConfigValue('port')) // 3000
+
+// typeof - qiymatning turini olish uchun ishlatiladi
+// keyof - obyektning kalitlarini olish uchun ishlatiladi
+// keyof typeof - obyektning kalitlarini olish uchun ishlatiladi, lekin obyektning turini ham aniqlaydi
+
+// const user = {
+// 	name: 'John Doe',
+// 	age: 30,
+// 	isMarried: false,
+// }
+
+// type User = typeof user
+
+// function logUser(user: User) {
+// 	console.log(user)
+// }
+
+// logUser(user)
+
+// type IUser = {
+// 	name: string
+// 	age: number
+// 	isMarried: boolean
+// }
+
+// type UserKeys = keyof IUser
+
+// function getUserKey(key: UserKeys) {
+// 	console.log(key)
+// }
+
+// getUserKey('age')
+
+// function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+// 	return obj[key]
+// }
+
+// const car = {
+// 	modal: 'Toyota',
+// 	year: 2020,
+// 	color: 'red',
+// }
+
+// const userName = getProperty(car, 'year')
