80542c421c44dfbdcd9b26002956293685787735
Muhammad <mitcao.000@gmail.com>
2025-06-11 10:12:01 +0300

    47-Method-decorator

--- app.js
+++ app.js
@@ -5,33 +5,48 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function CreateAt(constructor) {
-    return class extends constructor {
-        constructor() {
-            super(...arguments);
-            this.createdAt = new Date();
+function Logger(target, propertyKey, descriptor) {
+    descriptor.value = function (...args) {
+        console.log('Method not implemented. ');
+        return args;
+    };
+    return descriptor;
+}
+function Auth(role) {
+    return function (target, propertyKey, descriptor) {
+        if (role === 'admin') {
+            throw new Error('Only Admins can access this method.');
         }
+        const originalMethod = descriptor.value;
+        descriptor.value = function (...args) {
+            if (!this.isAdmin) {
+                console.log('Access denied: You are not an admin.');
+                return;
+            }
+            return originalMethod.apply(this, args);
+        };
+        return descriptor;
     };
 }
-let Course = class Course {
-    constructor() {
-        this.name = 'TypeScript Course';
-        this.excerpt = 'Learn TypeScript from scratch';
+class User {
+    constructor(name, age, isAdmin) {
+        this.name = name;
+        this.age = age;
+        this.isAdmin = isAdmin;
     }
-};
-Course = __decorate([
-    CreateAt
-], Course);
-let Lesson = class Lesson {
-    constructor() {
-        this.name = 'Introduction to TypeScript';
-        this.excerpt = 'Understanding the basics of TypeScript';
+    greeting() {
+        throw new Error('Method not implemented.');
     }
-};
-Lesson = __decorate([
-    CreateAt
-], Lesson);
-const cours = new Course();
-const lesson = new Lesson();
-console.log(cours.createdAt);
-console.log(lesson);
+    deleteUser() {
+        console.log('Deleting user');
+    }
+}
+__decorate([
+    Logger
+], User.prototype, "greeting", null);
+__decorate([
+    Auth('admin')
+], User.prototype, "deleteUser", null);
+const user = new User('John', 30, false);
+user.greeting(); // Logs: Method not implemented.
+user.deleteUser(); // Logs: Access denied: You are not an admin.
--- app.ts
+++ app.ts
@@ -1,25 +1,59 @@
-function CreateAt<T extends { new (...args: any[]): {} }>(constructor: T) {
-	return class extends constructor {
-		readonly createdAt = new Date()
+function Logger(
+	target: any,
+	propertyKey: string,
+	descriptor: PropertyDescriptor
+) {
+	descriptor.value = function (...args: any[]) {
+		console.log('Method not implemented. ')
+		return args
+	}
+
+	return descriptor
+}
+
+function Auth(role: 'admin' | 'user') {
+	return function (
+		target: any,
+		propertyKey: string,
+		descriptor: PropertyDescriptor
+	) {
+		if (role === 'admin') {
+			throw new Error('Only Admins can access this method.')
+		}
+
+		const originalMethod = descriptor.value
+
+		descriptor.value = function (this: { isAdmin: boolean }, ...args: any[]) {
+			if (!this.isAdmin) {
+				console.log('Access denied: You are not an admin.')
+				return
+			}
+
+			return originalMethod.apply(this, args)
+		}
+
+		return descriptor
 	}
 }
 
-@CreateAt
-class Course {
-	name = 'TypeScript Course'
-	excerpt = 'Learn TypeScript from scratch'
+class User {
+	constructor(
+		public name: string,
+		public age: number,
+		public isAdmin: boolean
+	) {}
+
+	@Logger
+	greeting() {
+		throw new Error('Method not implemented.')
+	}
+
+	@Auth('admin')
+	deleteUser() {
+		console.log('Deleting user')
+	}
 }
 
-@CreateAt
-class Lesson {
-	name = 'Introduction to TypeScript'
-	excerpt = 'Understanding the basics of TypeScript'
-}
-
-type CreateEntity = { createdAt: Date }
-
-const cours = new Course() as Course & CreateEntity
-const lesson = new Lesson() as Lesson & CreateEntity
-
-console.log(cours.createdAt)
-console.log(lesson)
+const user = new User('John', 30, false)
+user.greeting() // Logs: Method not implemented.
+user.deleteUser() // Logs: Access denied: You are not an admin.
