a8e5d2c0098a66fff98c18908b6611fc76b96b7c
Muhammad <mitcao.000@gmail.com>
2025-06-11 17:15:21 +0300

    50-Declare

--- app.ts
+++ app.ts
@@ -1,135 +1,14 @@
-// |-----------------------------------------------| cache Decorators |--------------------------------------------------------|
+import { add, multiply, subtract } from 'math'
 
-function CacheResult(
-	target: any,
-	propertyKey: string | symbol,
-	descriptor: PropertyDescriptor
-) {
-	const originalMethod = descriptor.value
-	const cache = new Map<string, any>()
+console.log(add(5, 10)) // Output: 15
+console.log(subtract(20, 30)) // Output: 50
+console.log(multiply(2, 3)) // Output: 6
 
-	descriptor.value = function (...args: any[]) {
-		const key = JSON.stringify(args)
+const myCar = new Car('Toyota', 'Corolla', 2020)
+console.log(myCar.getDetails()) // Output: Toyota Corolla 2020
 
-		if (cache.has(key)) {
-			console.log(`Cache hit for ${String(propertyKey)} with args ${key}`)
-			return cache.get(key)
-		}
+// |-----------------------------------------------| Declaration File |--------------------------------------------------------|
 
-		console.log(`Cache miss for ${String(propertyKey)} with args ${key}`)
-		const result = originalMethod.apply(this, args)
-		cache.set(key, result)
-		return result
-	}
+// declare function trackEvent(name: string, data: { userId: string }): void
 
-	return descriptor
-}
-
-class MathService {
-	@CacheResult
-	factorial(n: number): number {
-		if (n <= 1) return 1
-		return n * this.factorial(n - 1)
-	}
-}
-
-const mathService = new MathService()
-console.log(mathService.factorial(5)) // Cache miss, computes result
-console.log(mathService.factorial(5)) // Cache hit, returns cached result
-
-// |-----------------------------------------------| Parameter Decorators |----------------------------------------------------|
-
-// function LogParameter(
-// 	target: any,
-// 	propertyKey: string,
-// 	parameterIndex: number
-// ) {
-// 	console.log(
-// 		`Parameter decorator called for ${propertyKey} at index ${parameterIndex}`
-// 	)
-// }
-
-// class LogService {
-// 	createLog(name: string, @LogParameter id: string) {
-// 		console.log(`Log created: ${name} with ID: ${id}`)
-// 	}
-// }
-
-// const logService = new LogService()
-// logService.createLog('UserLogin', '123100')
-
-// |-----------------------------------------------| Accessor Decorators |-----------------------------------------------------|
-
-// function Readonly(
-// 	target: any,
-// 	propertyKey: string,
-// 	descriptor: PropertyDescriptor
-// ) {
-// 	descriptor.set = function () {
-// 		throw new Error(`Cannot set value for read-only property: ${propertyKey}`)
-// 	}
-// }
-
-// class Product {
-// 	private _id: number
-
-// 	@Readonly
-// 	get id() {
-// 		return this._id
-// 	}
-// }
-
-// const product = new Product()
-// console.log(product.id)
-// // @ts-ignore
-// product.id = 123
-
-// |-----------------------------------------------| Property Decorators |-----------------------------------------------------|
-
-// function Uppercase(target: any, propertyKey: string) {
-// 	let value: string
-
-// 	const getter = () => value
-// 	const setter = (newValue: string) => {
-// 		value = newValue.toUpperCase()
-// 	}
-
-// 	Object.defineProperty(target, propertyKey, {
-// 		get: getter,
-// 		set: setter,
-// 		enumerable: true,
-// 		configurable: true,
-// 	})
-// }
-// function Lowercase(target: any, propertyKey: string) {
-// 	let value: string
-
-// 	const getter = () => value
-// 	const setter = (newValue: string) => {
-// 		value = newValue.toLowerCase()
-// 	}
-
-// 	Object.defineProperty(target, propertyKey, {
-// 		get: getter,
-// 		set: setter,
-// 		enumerable: true,
-// 		configurable: true,
-// 	})
-// }
-
-// class User {
-// 	@Uppercase
-// 	public firstName: string
-
-// 	@Lowercase
-// 	public lastName: string
-
-// 	constructor(firstName: string, lastName: string) {
-// 		this.firstName = firstName
-// 		this.lastName = lastName
-// 	}
-// }
-
-// const user = new User('John', 'Doe')
-// console.log(user.firstName)
-// console.log(user.lastName)
+// trackEvent('user_login', { userId: '12345' })
