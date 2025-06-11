236e4f8e4f14d7aca8a51a443ba8505b89526d15
Muhammad <mitcao.000@gmail.com>
2025-06-11 09:35:38 +0300

    46-CreatedAt-decorator

--- app.js
+++ app.js
@@ -5,40 +5,33 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function ChangeShape(constructor) {
+function CreateAt(constructor) {
     return class extends constructor {
         constructor() {
             super(...arguments);
-            this.name = 'Circle';
-            this.color = 'red';
-        }
-        getInfo() {
-            return this.name + ' ' + this.color;
+            this.createdAt = new Date();
         }
     };
 }
-function WithVersion(version) {
-    return function (constructor) {
-        return class extends constructor {
-            constructor() {
-                super(...arguments);
-                this.version = version;
-            }
-        };
-    };
-}
-let Circle = class Circle {
+let Course = class Course {
     constructor() {
-        this.name = 'Circle';
-        console.log('Circle created');
-    }
-    getValue() {
-        return this.name;
+        this.name = 'TypeScript Course';
+        this.excerpt = 'Learn TypeScript from scratch';
     }
 };
-Circle = __decorate([
-    ChangeShape,
-    WithVersion('1.0.0')
-], Circle);
-const shape = new Circle();
-console.log(shape);
+Course = __decorate([
+    CreateAt
+], Course);
+let Lesson = class Lesson {
+    constructor() {
+        this.name = 'Introduction to TypeScript';
+        this.excerpt = 'Understanding the basics of TypeScript';
+    }
+};
+Lesson = __decorate([
+    CreateAt
+], Lesson);
+const cours = new Course();
+const lesson = new Lesson();
+console.log(cours.createdAt);
+console.log(lesson);
--- app.ts
+++ app.ts
@@ -1,43 +1,25 @@
-interface IShape {
-	name: string
-	getValue(): string
-}
-
-function ChangeShape<TBase extends { new (...args: any[]): {} }>(
-	constructor: TBase
-) {
+function CreateAt<T extends { new (...args: any[]): {} }>(constructor: T) {
 	return class extends constructor {
-		name: string = 'Circle'
-		color: string = 'red'
-		getInfo() {
-			return this.name + ' ' + this.color
-		}
+		readonly createdAt = new Date()
 	}
 }
 
-function WithVersion(version: '1.0.0' | '2.0.0') {
-	return function <TBase extends { new (...args: any[]): {} }>(
-		constructor: TBase
-	) {
-		return class extends constructor {
-			version: string = version
-		}
-	}
+@CreateAt
+class Course {
+	name = 'TypeScript Course'
+	excerpt = 'Learn TypeScript from scratch'
 }
 
-@ChangeShape
-@WithVersion('1.0.0')
-class Circle implements IShape {
-	name: string = 'Circle'
-
-	constructor() {
-		console.log('Circle created')
-	}
-
-	getValue(): string {
-		return this.name
-	}
+@CreateAt
+class Lesson {
+	name = 'Introduction to TypeScript'
+	excerpt = 'Understanding the basics of TypeScript'
 }
 
-const shape = new Circle()
-console.log(shape)
+type CreateEntity = { createdAt: Date }
+
+const cours = new Course() as Course & CreateEntity
+const lesson = new Lesson() as Lesson & CreateEntity
+
+console.log(cours.createdAt)
+console.log(lesson)
